a490a77a83dc714a0ce74323b766f9e2dc35b60c
Jonathan LaCour <jonathan@cleverdevil.org>
2017-03-31 05:45:18 +0000

    Made it possible to have a food/drink log without a body. Properly set the default category to 'ate'.

--- Food.php
+++ Food.php
@@ -81,75 +81,75 @@
                     $new = false;
                 }
                 $body = \Idno\Core\site()->currentPage()->getInput('body');
-                if (!empty($body)) {
 
-                    $this->body            = $body;
-                    $this->title           = \Idno\Core\site()->currentPage()->getInput('title');
-                    $this->category        = \Idno\Core\site()->currentPage()->getInput('category');
-                    $access                = \Idno\Core\site()->currentPage()->getInput('access');
-                    $this->setAccess($access);
+                $this->body            = $body;
+                $this->title           = \Idno\Core\site()->currentPage()->getInput('title');
+                $this->category        = \Idno\Core\site()->currentPage()->getInput('category');
+                $access                = \Idno\Core\site()->currentPage()->getInput('access');
+                $this->setAccess($access);
 
-                    if ($time = \Idno\Core\site()->currentPage()->getInput('created')) {
-                        if ($time = strtotime($time)) {
-                            $this->created = $time;
-                        }
+                if (empty($this->category)) {
+                    $this->category = 'ate';
+                }
+
+                if ($time = \Idno\Core\site()->currentPage()->getInput('created')) {
+                    if ($time = strtotime($time)) {
+                        $this->created = $time;
                     }
-    
-                    if ($new) {
-                        if (!empty($_FILES['photo']['tmp_name'])) {
-                            if (\Idno\Entities\File::isImage($_FILES['photo']['tmp_name'])) {
-                                
-                                // Extract exif data so we can rotate
-                                if (is_callable('exif_read_data') && $_FILES['photo']['type'] == 'image/jpeg') {
-                                    try {
-                                        if (function_exists('exif_read_data')) {
-                                            if ($exif = exif_read_data($_FILES['photo']['tmp_name'])) {
-                                                $this->exif = base64_encode(serialize($exif)); // Yes, this is rough, but exif contains binary data that can not be saved in mongo
-                                            }
+                }
+
+                if ($new) {
+                    if (!empty($_FILES['photo']['tmp_name'])) {
+                        if (\Idno\Entities\File::isImage($_FILES['photo']['tmp_name'])) {
+                            
+                            // Extract exif data so we can rotate
+                            if (is_callable('exif_read_data') && $_FILES['photo']['type'] == 'image/jpeg') {
+                                try {
+                                    if (function_exists('exif_read_data')) {
+                                        if ($exif = exif_read_data($_FILES['photo']['tmp_name'])) {
+                                            $this->exif = base64_encode(serialize($exif)); // Yes, this is rough, but exif contains binary data that can not be saved in mongo
                                         }
-                                    } catch (\Exception $e) {
-                                        $exif = false;
                                     }
-                                } else {
+                                } catch (\Exception $e) {
                                     $exif = false;
                                 }
-                                
-                                if ($photo = \Idno\Entities\File::createFromFile($_FILES['photo']['tmp_name'], $_FILES['photo']['name'], $_FILES['photo']['type'], true, true)) {
-                                    $this->attachFile($photo);
+                            } else {
+                                $exif = false;
+                            }
+                            
+                            if ($photo = \Idno\Entities\File::createFromFile($_FILES['photo']['tmp_name'], $_FILES['photo']['name'], $_FILES['photo']['type'], true, true)) {
+                                $this->attachFile($photo);
 
-                                    // Now get some smaller thumbnails, with the option to override sizes
-                                    $sizes = \Idno\Core\site()->events()->dispatch('photo/thumbnail/getsizes', new \Idno\Core\Event(array('sizes' => array('large' => 800, 'medium' => 400, 'small' => 200))));
-                                    $eventdata = $sizes->data();
-                                    foreach ($eventdata['sizes'] as $label => $size) {
+                                // Now get some smaller thumbnails, with the option to override sizes
+                                $sizes = \Idno\Core\site()->events()->dispatch('photo/thumbnail/getsizes', new \Idno\Core\Event(array('sizes' => array('large' => 800, 'medium' => 400, 'small' => 200))));
+                                $eventdata = $sizes->data();
+                                foreach ($eventdata['sizes'] as $label => $size) {
 
-                                        $filename = $_FILES['photo']['name'];
+                                    $filename = $_FILES['photo']['name'];
 
-                                        if ($thumbnail = \Idno\Entities\File::createThumbnailFromFile($_FILES['photo']['tmp_name'], "{$filename}_{$label}", $size, false)) {
-                                            $varname        = "thumbnail_{$label}";
-                                            $this->$varname = \Idno\Core\site()->config()->url . 'file/' . $thumbnail;
+                                    if ($thumbnail = \Idno\Entities\File::createThumbnailFromFile($_FILES['photo']['tmp_name'], "{$filename}_{$label}", $size, false)) {
+                                        $varname        = "thumbnail_{$label}";
+                                        $this->$varname = \Idno\Core\site()->config()->url . 'file/' . $thumbnail;
 
-                                            $varname        = "thumbnail_{$label}_id";
-                                            $this->$varname = substr($thumbnail, 0, strpos($thumbnail, '/'));
-                                        }
+                                        $varname        = "thumbnail_{$label}_id";
+                                        $this->$varname = substr($thumbnail, 0, strpos($thumbnail, '/'));
                                     }
                                 }
-                            } else {
-                                \Idno\Core\site()->session()->addErrorMessage('This doesn\'t seem to be an image ..');
                             }
+                        } else {
+                            \Idno\Core\site()->session()->addErrorMessage('This doesn\'t seem to be an image ..');
                         }
                     }
+                }
 
-                    if ($this->save($new)) {
+                if ($this->save($new)) {
 
-                        $autosave = new Autosave();
-                        $autosave->clearContext('food');
+                    $autosave = new Autosave();
+                    $autosave->clearContext('food');
 
-                        \Idno\Core\Webmention::pingMentions($this->getURL(), \Idno\Core\site()->template()->parseURLs($this->getTitle() . ' ' . $this->getDescription()));
+                    \Idno\Core\Webmention::pingMentions($this->getURL(), \Idno\Core\site()->template()->parseURLs($this->getTitle() . ' ' . $this->getDescription()));
 
-                        return true;
-                    }
-                } else {
-                    \Idno\Core\site()->session()->addErrorMessage('You can\'t save an empty entry.');
+                    return true;
                 }
 
                 return false;
